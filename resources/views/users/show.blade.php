759bcc1543a379df7fc77213159ce4782faf7f33
shiding <536336091@qq.com>
2018-04-20 15:37:33 +0800

    '个人页面'

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -40,10 +40,13 @@
             </div>
             <hr>
 
-            {{-- 用户发布的内容--}}
             <div class="panel panel-default">
                 <div class="panel-body">
-                    暂无数据 ~_~
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a>Ta 的话题</a></li>
+                        <li class="active"><a>Ta 的回复</a></li>
+                    </ul>
+                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
                 </div>
             </div>
         </div>
