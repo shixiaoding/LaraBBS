0839adcc21b9b4e79435bb89ac154f96bfc346fe
shiding <536336091@qq.com>
2018-04-27 19:29:44 +0800

    ’页面显示‘

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -17,33 +17,55 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
-	{
-		$topics = $topic->withOrder($request->order)->paginate(30);
-		return view('topics.index', compact('topics'));
-	}
+    public function index(Request $request, Topic $topic)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(30);
+        return view('topics.index', compact('topics'));
+    }
 
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
-	    $categories = Category::all();
-		return view('topics.create_and_edit', compact('topic', 'categories'));
-	}
+    public function create(Topic $topic)
+    {
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function store(TopicRequest $request, Topic $topic)
-	{
+    public function store(TopicRequest $request, Topic $topic)
+    {
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '成功创建话题！');
 	}
 
-	//上传
+	public function edit(Topic $topic)
+	{
+        $this->authorize('update', $topic);
+		return view('topics.create_and_edit', compact('topic'));
+	}
+
+	public function update(TopicRequest $request, Topic $topic)
+	{
+		$this->authorize('update', $topic);
+		$topic->update($request->all());
+
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+	}
+
+	public function destroy(Topic $topic)
+	{
+		$this->authorize('destroy', $topic);
+		$topic->delete();
+
+		return redirect()->route('topics.index')->with('success', '成功删除！');
+	}
+
+    //上传
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
         //初始化数据
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
         <meta http-equiv="x-ua-compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('title', 'LaraBBS')</title>
+        <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+        <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @yield('styles')
     </head>
