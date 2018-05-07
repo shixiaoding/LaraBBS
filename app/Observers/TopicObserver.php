936f5cc3ceb96dc1987dcb79f71ed7f018c79c40
shiding <536336091@qq.com>
2018-05-07 20:17:18 +0800

    'xx'

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -20,7 +21,8 @@ class TopicObserver
 
         //seo url 翻译
         if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            //推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
