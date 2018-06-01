25585242930104ff887c6eb7c229a3db254ecfc8
shiding <53633609@qq.com>
2018-06-01 17:54:48 +0800

    '后台管理'

--- config/administrator/users.php
+++ config/administrator/users.php
@@ -7,15 +7,35 @@ return [
     'heading' =>  '用户管理',
     'single' => '用户',
     'model' => User::class,
+    'permission'=> function()
+    {
+        return Auth::user()->can('manage_users');
+    },
     'columns' => [
         'id' => [
             'title' => 'ID'
         ],
+        'avatar' => [
+            // 数据表格里列的名称，默认会使用『列标识』
+            'title'  => '头像',
+
+            // 默认情况下会直接输出数据，你也可以使用 output 选项来定制输出内容
+            'output' => function ($avatar, $model) {
+                return empty($avatar) ? 'N/A' : '<img src="'.$avatar.'" width="40">';
+            },
+
+            // 是否允许排序
+            'sortable' => false,
+        ],
         'name' => [
-            'title' => '用户名',
+            'title'    => '用户名',
+            'sortable' => false,
+            'output' => function ($name, $model) {
+                return '<a href="/users/'.$model->id.'" target=_blank>'.$name.'</a>';
+            },
         ],
         'email' => [
-            'title' => 'Email',
+            'title' => '邮箱',
         ],
         'created_at',
         'operation' => [
@@ -32,18 +52,42 @@ return [
             'type' => 'text'
         ],
         'email' => [
-            'title' => 'Email',
+            'title' => '邮箱',
+        ],
+        'password' => [
+            'title' => '密码',
+
+            // 表单使用 input 类型 password
+            'type' => 'password',
+        ],
+        'avatar' => [
+            'title' => '用户头像',
+
+            // 设置表单条目的类型，默认的 type 是 input
+            'type' => 'image',
+
+            // 图片上传必须设置图片存放路径
+            'location' => public_path() . '/uploads/images/avatars/',
+        ],
+        'roles' => [
+            'title'      => '用户角色',
+
+            // 指定数据的类型为关联模型
+            'type'       => 'relationship',
+
+            // 关联模型的字段，用来做关联显示
+            'name_field' => 'name',
         ],
     ],
     'filters' => [
         'id' => [
-            'title' => 'ID',
+            'title' => '用户 ID',
         ],
         'name' => [
             'title' => '用户名',
         ],
         'email' => [
-            'title' => 'Email',
+            'title' => '邮箱',
         ],
     ],
 ];
