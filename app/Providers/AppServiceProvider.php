20862af4cfb221999e0e588743b8eb3bfd9b34aa
shixiaoding <536336091@qq.com>
2018-06-09 17:01:24 +0800

    ‘数据库问题'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Carbon::setLocale('zh');
         Topic::observe(TopicObserver::class);
         Reply::observe(ReplyObserver::class);
